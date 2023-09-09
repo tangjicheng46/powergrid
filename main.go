cb7ce197f79c7e8bccc7159ff05dc84898d98767
tangjicheng <tangjch15@gmail.com>
2023-09-09 16:52:27 +0900

    chore: update sqlite example

--- main.go
+++ main.go
@@ -3,65 +3,88 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
-
-	_ "github.com/mattn/go-sqlite3" // 导入 SQLite 驱动程序
 )
 
 func main() {
-	// 打开或创建 SQLite 数据库文件
-	db, err := sql.Open("sqlite3", "mydatabase.db")
+	// 打开或创建数据库文件
+	db, err := sql.Open("sqlite3", "my-database.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	// 创建表
-	createTable := `
-	CREATE TABLE IF NOT EXISTS users (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username TEXT,
-		email TEXT
-	);
-	`
-	_, err = db.Exec(createTable)
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS my_table (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT,
+			age INTEGER
+		)
+	`)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// 插入数据
-	insertData := `
-	INSERT INTO users (username, email) VALUES (?, ?);
-	`
-	_, err = db.Exec(insertData, "user1", "user1@example.com")
+	insertStatement := "INSERT INTO my_table (name, age) VALUES (?, ?)"
+	_, err = db.Exec(insertStatement, "Alice", 30)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(insertStatement, "Bob", 25)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// 查询数据
-	query := `
-	SELECT id, username, email FROM users;
-	`
-	rows, err := db.Query(query)
+	rows, err := db.Query("SELECT id, name, age FROM my_table")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
-	// 处理查询结果
+	fmt.Println("Query results:")
 	for rows.Next() {
-		var id int
-		var username string
-		var email string
-		err := rows.Scan(&id, &username, &email)
+		var id, age int
+		var name string
+		err := rows.Scan(&id, &name, &age)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("ID: %d, Username: %s, Email: %s\n", id, username, email)
+		fmt.Printf("ID: %d, Name: %s, Age: %d\n", id, name, age)
 	}
 
-	// 检查是否有错误
-	if err := rows.Err(); err != nil {
+	// 修改数据
+	updateStatement := "UPDATE my_table SET age = ? WHERE name = ?"
+	_, err = db.Exec(updateStatement, 28, "Alice")
+	if err != nil {
 		log.Fatal(err)
+	}
+
+	// 删除数据
+	deleteStatement := "DELETE FROM my_table WHERE name = ?"
+	_, err = db.Exec(deleteStatement, "Bob")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 再次查询数据以查看修改和删除的效果
+	rows, err = db.Query("SELECT id, name, age FROM my_table")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	fmt.Println("Updated query results:")
+	for rows.Next() {
+		var id, age int
+		var name string
+		err := rows.Scan(&id, &name, &age)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %d, Name: %s, Age: %d\n", id, name, age)
 	}
 }
